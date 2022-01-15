edd21cd6d9f99943460f03cfbfe361dd024803cd
Wolfensteijn <wolf.schouten+github@nelen-schuurmans.nl>
2022-01-15 21:48:03 +0100

    trying random shit out

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -109,7 +109,8 @@ _latest_version=$(basename $(cat ./_latest_release | grep -wo "NginxProxyManager
 # Download nginx-proxy-manager source
 log "Downloading NPM v$_latest_version"
 # runcmd 'wget $WGETOPT -c $_npm_url/archive/v$_latest_version.tar.gz -O - | tar -xz'
-cd /nginx-proxy-manager-2.9.14
+sleep 1
+cd /nginx-proxy-manager-$_latest_version
 
 log "Setting up enviroment"
 # Crate required symbolic links
